5b54229ebe39393d13bcca75db728f19dc898a5b
chanchantal <maria.chantal@edu.uah.es>
2021-11-12 10:04:31 +0100

    no he hecho nada pero toma je

--- Practicas/Practica2/LaBusquedaDelTesoroParser.java
+++ Practicas/Practica2/LaBusquedaDelTesoroParser.java
@@ -1,4 +1,4 @@
-// Generated from LaBusquedaDelTesoro.g4 by ANTLR 4.7.2
+// Generated from LaBusquedaDelTesoro.g4 by ANTLR 4.9.2
 import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.*;
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class LaBusquedaDelTesoroParser extends Parser {
-	static { RuntimeMetaData.checkVersion("4.7.2", RuntimeMetaData.VERSION); }
+	static { RuntimeMetaData.checkVersion("4.9.2", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =
@@ -88,6 +88,7 @@ public class LaBusquedaDelTesoroParser extends Parser {
 		super(input);
 		_interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
 	}
+
 	public static class ProgContext extends ParserRuleContext {
 		public List<ExprContext> expr() {
 			return getRuleContexts(ExprContext.class);
